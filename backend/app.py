50c2f699ba3767c7e0c770b71dd9d160d7570b17
khushikishan <khushi.kishan2004@gmail.com>
2025-11-13 21:29:22 -0800

    modified app.py

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,12 @@
-# api imports
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-# router imports
-from routers import movieRouter
+from routers import (
+    movieRouter,
+    reviewRouter,
+    userRouter,
+    adminRouter,
+    listsRouter
+)
 
 app = FastAPI(
     title="BestBytes Movie Review API",
@@ -10,3 +14,21 @@ app = FastAPI(
     version="1.0.0",
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(movieRouter.router, prefix="/movies", tags=["Movies"])
+app.include_router(reviewRouter.router, prefix="/reviews", tags=["Reviews"])
+app.include_router(userRouter.router, prefix="/users", tags=["Users"])
+app.include_router(adminRouter.router, prefix="/admin", tags=["Admin"])
+app.include_router(listsRouter.router, prefix="/lists", tags=["Lists"])
+
+@app.get("/")
+def root():
+    """endpoint working"""
+    return {"message": "API running"}
