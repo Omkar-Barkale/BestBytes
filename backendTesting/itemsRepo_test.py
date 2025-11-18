d9b63311ee66f2331c213655317e3792610a02b1
Omkar Barkale <obarkale@student.ubc.ca>
2025-11-18 12:04:56 -0800

    String had wrong format, now has
    String formatted to match a path, works now

--- backendTesting/itemsRepo_test.py
+++ backendTesting/itemsRepo_test.py
@@ -6,4 +6,5 @@ from backend.repositories import itemsRepo
 
 #pylint: disable = function-naming-style, method-naming-style
 def test_GetMovie():
-    assert(itemsRepo.getMovieDir("test")) == Path.cwd() + "/backend/data"
+    assert(str(itemsRepo.getMovieDir("test"))) == str(Path.cwd()) + "\\backend\data\\test"
+    
