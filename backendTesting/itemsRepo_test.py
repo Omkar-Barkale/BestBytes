03567a88a80e98d8a7a763ba38cdb4dc16aa7856
Omkar Barkale <obarkale@student.ubc.ca>
2025-11-18 11:29:32 -0800

    Added a tester testfunction, just to see what how it works

---
+++ backendTesting/itemsRepo_test.py
@@ -0,0 +1,9 @@
+import pytest
+import sys
+from pathlib import Path
+
+from backend.repositories import itemsRepo
+
+#pylint: disable = function-naming-style, method-naming-style
+def test_GetMovie():
+    assert(itemsRepo.getMovieDir("test")) == Path.cwd() / "data"
