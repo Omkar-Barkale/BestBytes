80579ae4eb781524180a13a6ec26b0012bc4aa7d
khushikishan <khushi.kishan2004@gmail.com>
2025-11-21 13:04:20 -0800

    reads users from json file into the dict edited

--- backend/services/movieListServices.py
+++ backend/services/movieListServices.py
@@ -29,27 +29,15 @@ def saveMovieList(list : List[movie], user: str, listName: str, path: Path):
         json.dump(data,jsonFile)
         jsonFile.close()
 
-def saveAllUsers(users: Dict[str, Dict], path: Path):
-    """Save all users to user.json."""
-    path.mkdir(parents=True, exist_ok=True)
-    file_path = path / "user.json"
-
-    with open(file_path, "w") as f:
-        json.dump(users, f, indent=4)
-
-
-def readAllUsers(path: Path) -> Dict[str, Dict]:
-    """Read all users from user.json and return as a dict."""
+def readAllMovieList(path:Path) -> Dict[str, Dict[str, List[str]]]:
     data = {}
-    path.mkdir(parents=True, exist_ok=True)
-    file_path = path / "user.json"
+    path.mkdir(parents= True, exist_ok= True)
+    path = path/"movieLists.json"
 
-    if file_path.exists():
-        try:
-            with open(file_path, "r") as f:
-                data = json.load(f)
-        except json.JSONDecodeError:
-            data = {}
-
+    if path.exists():
+        with open(path,'r+') as jsonFile:
+            try:
+                data = json.load(jsonFile)
+            except json.JSONDecodeError:
+                 data = {}
     return data
-
