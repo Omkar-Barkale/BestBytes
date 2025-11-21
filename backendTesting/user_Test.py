0aa1515be1b4cb268b0ec611c2df635b270c142e
Omkar-Barkale <obarkale@student.ubc.ca>
2025-11-20 23:07:32 -0800

    Created checkusername tests

--- backendTesting/user_Test.py
+++ backendTesting/user_Test.py
@@ -3,7 +3,6 @@ import sys
 from pathlib import Path
 import json
 from backend.users import user
-
 from unittest.mock import mock_open, patch, MagicMock
 
 
@@ -13,25 +12,20 @@ email = "email@email.com"
 pswd = "password"
 testUser = user.User(name, email, pswd, save = False)
 
-def test_saveUserToDB():
-    #If we call this function, we should be able to find the specific user created
-    path = Path(r"backendTesting\usersTest.json")
-    with patch.object(testUser, 'path', path):
-        testUser.saveUserToDB(path)
-    with open(path,'r') as jsonFile:
-        try:
-            data = json.load(jsonFile) 
-        except json.JSONDecodeError:
-            data = {}
-        
-    assert name in data
-    assert email in data[name]["email"]
-    assert testUser.passwordHash in data[name]["password"]
 
-    #clean up
-    with open(path,'w') as jsonFile:
-        jsonFile.truncate(0)
+#Test checkUsername
+class TestCheckUsername:
+    def testCheckUsernameTooShort(self):
+        assert testUser.checkUsername("a") == False
 
+    def testCheckUsernameTooLong(self):
+        assert testUser.checkUsername("aaaaaaaaaaaaaaaaaaaaaaaaaa") == False
+
+    def testCheckUsernameNonAlphnum(self):
+        assert testUser.checkUsername("abcde%") == False
+    
+    def testCheckUsernameValid(self):
+        assert testUser.checkUsername("Username") == True
 
 
     
